99d5916a8194dafd83ca2e21f1746bdd9dee8c13
MiLL4U <aphiloboe@gmail.com>
2024-03-15 18:04:42 +0900

    add post list

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
+
+
+def post_list(request: HttpRequest) -> HttpResponse:
+    return render(request, 'blog/post_list.html', {})
