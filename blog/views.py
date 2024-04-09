cbd444b7e218f34a89958b86cb58bfcda58f630d
MiLL4U <aphiloboe@gmail.com>
2024-04-09 21:41:10 +0900

    add view for detailed blog post

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 
 from .models import Post
@@ -11,3 +11,8 @@ def post_list(request: HttpRequest) -> HttpResponse:
     posts = Post.objects.filter(
         published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request: HttpRequest, pk: int):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
