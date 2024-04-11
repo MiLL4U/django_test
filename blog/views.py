9e01a41bdfabcf607778acc3c0101a17abe7f299
MiLL4U <aphiloboe@gmail.com>
2024-04-11 15:35:17 +0900

    add edit post function

--- blog/views.py
+++ blog/views.py
@@ -31,3 +31,18 @@ def post_new(request: HttpRequest) -> HttpResponse:
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_edit(request: HttpRequest, pk: int) -> HttpResponse:
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user  # type: ignore
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
