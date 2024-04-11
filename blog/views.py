cd734f9d0f8a53322bd4307b3276658130f85406
MiLL4U <aphiloboe@gmail.com>
2024-04-11 14:37:09 +0900

    implement new post function

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,9 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404, redirect, render
 from django.utils import timezone
 
-from .models import Post
 from .forms import PostForm
+from .models import Post
 
 # Create your views here.
 
@@ -20,5 +20,14 @@ def post_detail(request: HttpRequest, pk: int):
 
 
 def post_new(request: HttpRequest):
-    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
