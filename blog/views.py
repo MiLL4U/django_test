75a02c879115e219acfe9ac6be62610c929fa33b
MiLL4U <aphiloboe@gmail.com>
2024-04-15 13:17:33 +0900

    add draft list

--- blog/views.py
+++ blog/views.py
@@ -44,3 +44,9 @@ def post_edit(request: HttpRequest, pk: int) -> HttpResponse:
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_draft_list(request: HttpRequest) -> HttpResponse:
+    posts = Post.objects.filter(
+        published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
