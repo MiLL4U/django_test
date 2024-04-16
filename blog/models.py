2c5fb662d14c6778174fae0815bf63cd409b9085
MiLL4U <aphiloboe@gmail.com>
2024-04-16 17:14:48 +0900

    add comment model

--- blog/models.py
+++ blog/models.py
@@ -17,3 +17,19 @@ class Post(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        'blog.Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=False)
+    is_approved = models.BooleanField(default=False)
+
+    def approve(self) -> None:
+        self.is_approved = True
+        self.save()
+
+    def __str__(self) -> str:
+        return self.text
